2639ec2f2d670ab2354c4e3b98ab3a78be32bf65
Zoneam <zoneam@gmail.com>
2022-08-10 12:32:21 -0700

    Median of Two Sorted Arrays

---
+++ 4 Median of Two Sorted Arrays.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+ var findMedianSortedArrays = function(nums1, nums2) {
+    let arr = [...nums1,...nums2].sort((a,b)=> a - b);
+        if(arr.length % 2 === 0){
+            return (arr[(arr.length/2)-1] + arr[arr.length/2])/2
+        }
+        return arr[Math.floor(arr.length/2)]
+};
