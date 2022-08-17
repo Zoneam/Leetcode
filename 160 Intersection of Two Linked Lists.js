1a552db2e2c702334fc4ebef530600e5e0be12a4
Zoneam <zoneam@gmail.com>
2022-08-16 17:17:05 -0700

    Intersection of Two Linked Lists

---
+++ 160 Intersection of Two Linked Lists.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+ var getIntersectionNode = function(headA, headB) {
+    if (!headA || !headB) return null; 
+    
+    let nowA = headA;
+    let nowB = headB;
+
+    while (nowA !== nowB) {
+        nowA = nowA ? nowA.next : headB;
+        nowB = nowB ? nowB.next : headA;
+    }
+
+    return nowA;
+
+};
